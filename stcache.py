a4ce2ade4ed197be4c29699ec5635ba19bc27765
meawoppl <meawoppl@gmail.com>
2021-08-16 18:20:20 -0700

    Working!

--- stcache.py
+++ stcache.py
@@ -1,11 +1,16 @@
+#!/usr/bin/env python3
+
 import datetime
 import requests
+import time
 
 from rush.quota import Quota
 from rush.throttle import Throttle
 from rush.limiters.periodic import PeriodicLimiter
+from rush.stores.dictionary import DictionaryStore as RushDictionaryStore
 
-ST_CACHE_URL = ""
+
+ST_CACHE_URL = "https://api.txcl.io/tle/day"
 DATE_FMT = "%Y-%m-%d"
 
 class TLEClient:
@@ -33,18 +38,27 @@ class TLEClient:
             sleep_time = max(sleep_time, hour_limit.retry_after.total_seconds())
 
         if sleep_time > 0:
-            self._ratelimit_wait(sleep_time)
+            time.sleep(sleep_time)
 
 
-    def get_tle_for_day(dt: datetime.datetime):
+    def get_tle_for_day(self, dt: datetime.datetime):
         request = {
             "identity": self._ident,
             "password": self._pass,
             "date": dt.strftime(DATE_FMT)
         }
-        resp_json = requests.post(URL, json=request).json()
-
+        resp_json = requests.post(ST_CACHE_URL, json=request).json()
+        print(resp_json)
         if not resp_json["cached"]:
             self._ratelimit_pause()
         
         return resp_json["tle"]
+
+if __name__ == "__main__":
+    import os
+    un = os.environ["ST_UN"] or input("ST Username:")
+    pw = os.environ["ST_PW"] or input("ST Password:")
+    date = "2000-01-01" or input("YYYY-MM-DD:")
+    dt = datetime.datetime.strptime(date, DATE_FMT)
+
+    print(TLEClient(un, pw).get_tle_for_day(dt))
